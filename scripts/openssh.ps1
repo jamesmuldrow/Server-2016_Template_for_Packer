194a5f5fbb3ad5ae1431e1bd575ab3f57d1b7bf6
James Muldrow <jamesmuldrow@gmail.com>
2019-11-26 19:11:06 -0500

    Updated openssh file

--- scripts/openssh.ps1
+++ scripts/openssh.ps1
@@ -1,5 +1,15 @@
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-Start-Service "sshd"
-Set-Service -Name sshd -StartupType 'Automatic'
-
-
+cd C:\Users\Administrator\Desktop
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v7.7.2.0p1-Beta/OpenSSH-Win64.zip" -OutFile "powershell.zip"
+Add-Type -assembly "system.io.compression.filesystem"
+[io.compression.zipfile]::ExtractToDirectory( 'C:\Users\Administrator\Desktop\powershell.zip','C:\Users\Administrator\Desktop' )
+$Acl = Get-Acl ".\OpenSSH-Win64"
+$Ar = New-Object  system.security.accesscontrol.filesystemaccessrule("users","FullControl","Allow")
+$Acl.SetAccessRule($Ar)
+Set-Acl ".\OpenSSH-Win64\*" $Acl
+cd .\OpenSSH-Win64
+.\install-sshd.ps1
+get-service | findstr ssh
+Start-Service sshd
+netsh advfirewall firewall add rule name=SSHPort dir=in action=allow protocol=TCP localport=22
+Set-Service -Name sshd -StartupType "Automatic"
